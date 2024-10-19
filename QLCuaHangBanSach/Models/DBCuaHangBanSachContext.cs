a5ed59c53b226a5dd07542c6297ff7981b98f88f
Linhhcutee <ljnkbgbg03@gmail.com>
2024-10-19 11:08:18 +0700

    updata connect strings

--- QLCuaHangBanSach/Models/DBCuaHangBanSachContext.cs
+++ QLCuaHangBanSach/Models/DBCuaHangBanSachContext.cs
@@ -33,7 +33,7 @@ namespace QLCuaHangBanSach.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("workstation id=dbqlbansach.mssql.somee.com;packet size=4096;user id=linhca_SQLLogin_1;pwd=t5s2j68z1g;data source=dbqlbansach.mssql.somee.com;persist security info=False;initial catalog=dbqlbansach;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("jdbc:mysql://localhost:3306/qlcuahangsach?user=cuahangsach&password=cuahangsach");
             }
         }
 
